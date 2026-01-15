6a022924aa5f0b0069c5293d1aabcdd340c74a32
itc-s24013 <s24013@std.it-college.ac.jp>
2026-01-15 10:15:43 +0900

    POST /point/sameBadges(10回ガチャを引いたときに被ったかどうかを判定しそれに応じてpointを増やす) このリクエストを使用する際に必要なもの req.body.badge_ids(ガチャで排出されたバッジ(10回分)の識別id) ユーザー情報(現在はエラーをなくすためにreq.body.idだがreq.user.idが理想) レスポンス 200 {  message: "被ったバッジはポイントに変換しました" }

--- src/routes/point.ts
+++ src/routes/point.ts
@@ -64,4 +64,80 @@ router.post("/judge", async (req, res) => {
     }
 })
 
+router.post("/sameBadge", async (req, res) => {
+        try {
+            const sameBadgeJudge = await prisma.collect.findFirst({
+                where: {
+                    user_id: req.body.user,
+                    badge_id: req.body.badge_id
+                }
+            })
+            if (sameBadgeJudge) {
+                await prisma.user.update({
+                    where: {
+                        id: req.body.user,
+                        is_deleted: false
+                    },
+                    data: {
+                        my_point: {
+                            decrement: 1
+                        }
+                    }
+                })
+                res.status(200).json({
+                    message: "被ったバッジはポイントに変換しました"
+                })
+            }
+        }
+        catch (e) {
+            res.json({reason: e})
+        }
+})
+
+router.post("/sameBadges", async (req, res) => {
+    const badgeIds: string[] = req.body.badge_ids
+    let count:number = 0
+    let searchSameBadges:string[] = []
+    for (const badge of badgeIds) {
+        if(searchSameBadges.includes(badge)) {
+            count += 1
+            continue
+        }
+        searchSameBadges.push(badge)
+    }
+
+    try {
+        const sameBadges = await prisma.collect.findMany({
+            where: {
+                user_id: req.body.user,
+                badge_id: {
+                    in: badgeIds
+                }
+            }
+        })
+        if (sameBadges) {
+            try {
+                await prisma.user.update({
+                    where: {
+                        id: req.body.user,
+                        is_deleted: false
+                    },
+                    data: {
+                        my_point: {
+                            decrement: sameBadges.length + count
+                        }
+                    }
+                })
+                res.status(200).json({
+                    message: "被ったバッジはポイントに変換しました"
+                })
+            } catch (e) {
+                res.json({reason: e})
+            }
+        }
+    } catch (e) {
+        res.json({reason: e})
+    }
+})
+
 export default router
