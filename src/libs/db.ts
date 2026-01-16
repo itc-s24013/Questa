d6fe27856f7b6822ccf70a27bc665974a37b82b4
itc-s24013 <s24013@std.it-college.ac.jp>
2026-01-16 16:24:20 +0900

    adapterの設定の記述方法を変更

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,17 +1,8 @@
 import {PrismaPg} from '@prisma/adapter-pg';
 import {PrismaClient} from '../generated/prisma/client.js'
 
-const url = String(process.env.DATABASE_URL)
-const params = url.match(
-  /^mysql:\/\/(?<user>.+?):(?<password>.+?)@(?<host>.+?):(?<port>\d+)\/(?<database>.+?)$/
-)?.groups || {}
-
 const adapter = new PrismaPg({
-  user: params.user,
-  password: params.password,
-  host: params.host,
-  port: Number(params.port),
-  database: params.database,
+  connectionString: process.env.DATABASE_URL,
 })
 const prisma = new PrismaClient({adapter})
 
