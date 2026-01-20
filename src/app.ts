3699f6dde45d49fd1072dfafb18da3db345f0f95
itc-s24013 <s24013@std.it-college.ac.jp>
2026-01-20 12:39:04 +0900

    GET /admin/users 現在存在するユーザーの一覧を返す。
    本来req.user.is_adminであるところをreq.body.is_adminにしている。

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
 import questRouter from './routes/quest.js'
+import adminRouter from './routes/admin.js'
 
 const app = express()
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/quest', questRouter)
+app.use('/admin', adminRouter)
 
 
 export default app
