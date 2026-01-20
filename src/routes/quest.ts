29cfbeacc1af6f4cad886611ef3227e7c9362514
itc-s24013 <s24013@std.it-college.ac.jp>
2026-01-20 12:23:12 +0900

    ユーザーが選択したクエストがクリア済みだった場合pointは10が返ってくるように変更した。 req.user.idが必要となるが今現在req.body.user_idと記述している。

--- src/routes/quest.ts
+++ src/routes/quest.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express'
+import  {Router} from 'express'
 import prisma from "../libs/db.js";
 
 export const router = Router();
@@ -34,19 +34,44 @@ router.get("/", async (req, res) => {
     }
 })
 
-    router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
+    try {
+        const quest = await prisma.quest.findUnique({
+            where: {
+                id: req.params.id,
+                is_deleted: false
+            }
+        })
+        if (!quest) {
+            res.status(404).json({reason: "存在しないクエストです。"})
+        }
         try {
-            return res.status(200).json(
-                await prisma.quest.findFirst({
-                    where: {
-                        id: req.params.id,
-                        is_deleted: false
-                    }
-                }))
-        }
-        catch (e) {
-            res.json({reason: e})
-        }
-    })
+            const cleared_quest = await prisma.clear.findFirst({
+                where: {
+                    user_id: req.body.user_id,
+                    quest_id: req.params.id
+                }
+            })
 
-    export default router
+            if (cleared_quest) {
+                res.status(200).json({
+                    id: quest?.id,
+                    title: quest?.title,
+                    choice1: quest?.choice1,
+                    choice2: quest?.choice2,
+                    choice3: quest?.choice3,
+                    choice4: quest?.choice4,
+                    point: 10
+                })
+            }
+
+            res.status(200).json(quest)
+        } catch (e) {
+            res.status(200).json({reason: e})
+        }
+    } catch (e) {
+        res.json({reason: e})
+    }
+})
+
+export default router
